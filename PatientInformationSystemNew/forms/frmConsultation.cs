1f6dc2a8a9864e2fea9f99ea007fc27af963bcb0
Joven Joshua C. Alovera <77560262+jovenalovera1999@users.noreply.github.com>
2021-10-25 18:29:58 +0800

    Get values from schedule module fix

--- PatientInformationSystemNew/forms/frmConsultation.cs
+++ PatientInformationSystemNew/forms/frmConsultation.cs
@@ -17,6 +17,29 @@ namespace PatientInformationSystemNew.forms
             InitializeComponent();
         }
 
+        components.Connections con = new components.Connections();
+        components.Values val = new components.Values();
+
+        private void frmConsultation_Load(object sender, EventArgs e)
+        {
+            this.txtPatientID.Text = val.PatientID;
+            this.txtFirstName.Text = val.PatientFirstName;
+            this.txtMiddleName.Text = val.PatientMiddleName;
+            this.txtLastName.Text = val.PatientLastName;
+            this.txtGender.Text = val.PatientGender;
+            this.txtAge.Text = val.PatientAge.ToString();
+            this.txtAddress.Text = val.PatientAddress;
+            this.dateBirthday.Value = val.PatientBirthday;
+            this.txtCellphoneNumber.Text = val.PatientCellphoneNumer;
+            this.txtTelephoneNumber.Text = val.PatientTelephoneNumber;
+            this.txtEmail.Text = val.PatientEmail;
+            this.txtHeight.Text = val.PatientHeight.ToString();
+            this.txtWeight.Text = val.PatientWeight.ToString();
+            this.txtTemperature.Text = val.PatientTemperature.ToString();
+            this.txtPulseRate.Text = val.PatientPulseRate.ToString();
+            this.txtBloodPressure.Text = val.PatientBloodPressure.ToString();
+        }
+
         private void btnPrescription_Click(object sender, EventArgs e)
         {
             
